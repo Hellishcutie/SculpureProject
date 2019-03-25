3184b8974e4f96ffbb0d76d916d9d05f40b969a7
Michael Wolf <michaelwolf95@gmail.com>
2019-03-24 23:58:10 -0700

    Added bobbing effect to cube.

---
+++ Cast in a Mad Light/Assets/BobbingEffect.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BobbingEffect : MonoBehaviour
+{
+    public float amplitude;
+    void Update()
+    {
+        float ypose = ((Mathf.Sin(Time.time) + 1)* 0.5f)*amplitude;
+        transform.localPosition = new Vector3(transform.localPosition.x, ypose, transform.localPosition.z);
+    }
+}
